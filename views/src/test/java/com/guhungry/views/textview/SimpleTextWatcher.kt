dd8eaa7cd7a0cc889b4a1a202a3ece50352c06d7
Woraphot Chokratanasombat <wc@asiola.com>
2018-07-29 00:03:51 +0700

    [SimpleTextWatcherTest] Change: Add unit test for SimpleTextWatcher

---
+++ views/src/test/java/com/guhungry/views/textview/SimpleTextWatcher.kt
@@ -0,0 +1,33 @@
+package com.guhungry.views.textview
+
+import com.guhungry.views.textview.SimpleTextWatcherTest.STRING
+import com.guhungry.views.toEditable
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.given
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.api.dsl.on
+
+object SimpleTextWatcherTest: Spek({
+    val sut = object : SimpleTextWatcher {}
+    given("a simple text watcher") {
+        on("beforeTextChanged") {
+            it("should do nothing when call ") {
+                sut.beforeTextChanged(STRING, 1, 2, 3)
+            }
+        }
+
+        on("onTextChanged") {
+            it("should do nothing when call ") {
+                sut.onTextChanged(STRING, 1, 2, 3)
+            }
+        }
+
+        on("afterTextChanged") {
+            it("should do nothing when call ") {
+                sut.afterTextChanged(STRING.toEditable())
+            }
+        }
+    }
+}) {
+    private const val STRING = "BEE"
+}
