85dff8abbe65a19bf7f2f6ed368c590d7039e0a5
Woraphot Chokratanasombat <wc@asiola.com>
2018-08-18 12:44:19 +0700

    [Main Activity] Change: Add example list

---
+++ app/src/main/java/com/guhungry/viewsexample/MainActivity.kt
@@ -0,0 +1,69 @@
+package com.guhungry.viewsexample
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+import com.guhungry.views.recyclerview.BindableViewHolder
+import com.guhungry.views.recyclerview.OnItemClickListener
+import com.guhungry.viewsexample.MainActivity.ExampleAdapter.ItemViewHolder
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.list_item_example.view.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setupList()
+    }
+
+    private val examples = listOf(
+            ExampleActivityModel(R.string.activity_load_more_adapter, LoadMoreAdapterActivity::class.java),
+            ExampleActivityModel(R.string.activity_linear_spacing_decorator, LinearSpacingActivity::class.java)
+    )
+
+    class ExampleActivityModel(val name: Int, val activity: Class<*>)
+
+    private fun setupList() {
+        list.layoutManager = GridLayoutManager(this, 2)
+        list.adapter = ExampleAdapter(examples).apply {
+            setOnItemClickListener(OnItemClickListener { startActivity(Intent(this@MainActivity, it)) })
+        }
+    }
+
+    inner class ExampleAdapter(private val list: List<ExampleActivityModel>) : RecyclerView.Adapter<ItemViewHolder>() {
+        private var onItemClickListener: OnItemClickListener<Class<*>>? = null
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+            return ItemViewHolder(layoutInflater.inflate(R.layout.list_item_example, parent, false))
+        }
+
+        override fun getItemCount() = list.size
+
+        override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+            holder.bindData(list[position])
+        }
+
+        fun setOnItemClickListener(listener: OnItemClickListener<Class<*>>) {
+            onItemClickListener = listener
+        }
+
+        inner class ItemViewHolder(view: View) : BindableViewHolder<ExampleActivityModel>(view) {
+            init {
+                itemView.setOnClickListener { onItemClickListener?.onItemClick(itemView.tag as Class<*>) }
+            }
+
+            override fun bindData(data: ExampleActivityModel) {
+                itemView.tag = data.activity
+
+                itemView.text1.setText(data.name)
+            }
+
+            override fun onRecycled() = Unit
+        }
+    }
+}
