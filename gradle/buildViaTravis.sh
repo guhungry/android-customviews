37508308fecee25054f43c1e8223321e17c70019
Woraphot Chokratanasombat <wc@asiola.com>
2018-08-19 23:22:59 +0700

    [Build Script] Change: Build only :views and add :views:jacocoTestReport

--- gradle/buildViaTravis.sh
+++ gradle/buildViaTravis.sh
@@ -3,14 +3,14 @@
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   echo -e "Build Pull Request #$TRAVIS_PULL_REQUEST => Branch [$TRAVIS_BRANCH]"
-  ./gradlew build
+  ./gradlew :views:build :views:jacocoTestReport
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" == "" ]; then
   echo -e 'Build Branch with Snapshot => Branch ['$TRAVIS_BRANCH']'
-  ./gradlew build  --stacktrace --info
+  ./gradlew :views:build :views:jacocoTestReport --stacktrace --info
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ]; then
   echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
-  ./gradlew build --stacktrace --info
+  ./gradlew :views:build :views:jacocoTestReport --stacktrace --info
 else
   echo -e 'WARN: Should not be here => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']  Pull Request ['$TRAVIS_PULL_REQUEST']'
-  ./gradlew build
+  ./gradlew :views:build :views:jacocoTestReport
 fi
